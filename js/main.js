595f7ca300239ada8eaba3bd7bc2a0332f64e1f0
Mariley20 <mariley2018@gmail.com>
2017-07-21 18:20:41 -0500

    modificando

--- js/main.js
+++ js/main.js
@@ -1,35 +1,16 @@
-reservas = document.getElementById('reserva');
-reservas.onclick = function(){
-   for (var i = 0; i < 32; i++) {
-      asientos[i] = undefined;
-   }
-};
+var asientos = []; // declaro array 
 
-var asientos = [{
-	nombre : "Maria",
-	apellido : "Cervantes",
-	dni : 77693456,
-	asiento : 3
-},
-	{
-		nombre : "Carlos",
-		apellido : "Fernadez",
-		dni : 99999999,
-		asiento : 29
-	},
-	{
-		nombre : "Katerine",
-		apellido : "Mendez",
-		dni : 12312312,
-		asiento : 30
-	}
-	];
+ function Pasajero(nombre, apellido, dni, nro_asiento){
+ 	this.nombre = nombre,
+ 	this.apellido = apellido,
+ 	this.dni = dni,
+ 	this.nro_asiento = nro_asiento
+ };
    
-function crearAsientos(){
-
-	var numeroAsiento = 32;
+function crearAsientos(asientos){ //creo mis asientos
+	var cantidadAsiento = 32;
 	var filas = 4;
-	var columnas = numeroAsiento / filas;
+	var columnas = cantidadAsiento / filas;
 	var pasillo="";
 		
 	var asientosHTML = "<table border='2'>";
@@ -37,41 +18,38 @@ function crearAsientos(){
 	for(var i = 1; i <= 4; i++ ){
 
 		asientosHTML += "<tr>";
-		var color = "";
-
-		var contar = i;
+		//var color = "";
+		var numeroDeAsiento = i;
 
 		for(var j = 0 ; j < columnas; j++){
-			(asientos[contar - 1] != undefined)? color = "bgcolor='#F70D0D'": color = "bgcolor='#2F5CED'";
-			asientosHTML += "<td "+ color +">"+ ( contar ) +"</td>";
-			contar += 4;
+
+			//(asientos[numeroDeAsiento - 1] != undefined)? color = "bgcolor='#F70D0D'": color = "bgcolor='#2F5CED'";
+			asientosHTML += "<td>"+ ( numeroDeAsiento ) +"</td>";
+			numeroDeAsiento += 4;
 		}
 
 		(i == 2 )? pasillo += "<br><td></td></br>":pasillo = "";
-
 		asientosHTML += "</tr>"+pasillo;
 	}
 	asientosHTML += "</table>";
-
 	document.getElementById('dibujarAsientos').innerHTML = asientosHTML;
 	//return asientosHTML;
 }
 
+
 crearAsientos();
-
-
-
-
+var colorCelda;
 var celdas = document.getElementsByTagName('td');
 for (var i = 0; i < celdas.length; i++) {
     celdas[i].addEventListener('click',redirect,false);
 }
 
 function redirect(event){
-
 	//alert(event.target.textContent);
 	//document.getElementById("mostrarNumeroAsiento").innerHTML=(event.target.textContent);
+
 	var asiento = event.target.textContent;
+	colorCelda = event.target;
 	//console.log(asiento);
 	var formReserva = "";
 	formReserva += "<h2>Reserva de Asiento </h2>"+
@@ -86,24 +64,35 @@ function redirect(event){
 					
 	var button = "<button onclick='reservarAsiento()'>Guardar Reserva</button>"
 	formReserva += button;
+	if(colorCelda.style.backgroundColor == "red"){
+		//alert(asiento);
+		for(var i = 0; i < asientos.length; i++){
+			if(asientos[i].nro_asiento == asiento){
+				nro_asiento.value = asiento;
+				nombre.innerHTML = asientos[i].nombre;
+				document.getElementById('apellido').value = asientos[i].apellido;
+				dni.value = asientos[i].dni;
+			}
+		}
+		console.log(nombre.value+"-"+apellido.value+"-"+dni.value+"-"+nro_asiento.value)
+	}*/
+	
 	document.getElementById("mostrarFormReserva").innerHTML = formReserva;
+
+
+
 }
 
 function reservarAsiento(){
 
 	var nro_asiento = document.getElementById("nro_asiento").value;
-	console.log(nro_asiento)
 	var nombre = document.getElementById('nombre').value;
 	var apellido = document.getElementById('apellido').value;
 	var dni = document.getElementById('dni').value;
-	//alert(nro_asiento +"-"+nombre+ "-" + apellido+"-"+dni)
-	asientos[nro_asiento - 1] = {
-		nombre : nombre,
-		apellido : apellido,
-		dni : dni,
-		asiento : nro_asiento
-	}
-	//crearAsientos();
+
+	var pasajeros  = new Pasajero(nombre, apellido, dni, nro_asiento);
+	asientos.push(pasajeros);
+	colorCelda.style.backgroundColor ="red"
 	console.log(asientos)
 }
 
@@ -112,12 +101,8 @@ function mostrarLista(){
 	listaPasajeros += "<tr><td colspan='2'>   Apellidos y Nombres   </td>"+
 						"<td>  N° DNI  </td><td>N° de Asiento</td></tr>";
 	for(var i = 0; i < asientos.length; i++){
-		if(asientos[i] != undefined){
-			listaPasajeros += "<tr><td>"+asientos[i].apellido+"</td><td>"+asientos[i].nombre+"</td>"+
-								"<td>"+asientos[i].dni+"</td><td>"+asientos[i].asiento+"</td></tr>";
-		}
-		
-		
+		listaPasajeros += "<tr><td>"+asientos[i].apellido+"</td><td>"+asientos[i].nombre+"</td>"+
+						"<td>"+asientos[i].dni+"</td><td>"+asientos[i].nro_asiento+"</td></tr>";
 	}
 	listaPasajeros += "</table>";
 document.getElementById('listaPasajeros').innerHTML = listaPasajeros;
@@ -125,17 +110,14 @@ document.getElementById('listaPasajeros').innerHTML = listaPasajeros;
 
 function buscarPasajero(){
 	dniBuscar = document.getElementById('buscaDNI').value;
-var buscarPasajeros = "<table>";
+	//console.log(dniBuscar + "-"+asientos[0].dni)
+	var buscarPasajeros = "<table>";
 	buscarPasajeros += "<tr><td colspan='2'>   Apellidos y Nombres   </td>"+
 						"<td>  N° DNI  </td><td>N° de Asiento</td></tr>";
 	for(var i = 0; i < asientos.length; i++){
-		if(asientos[i] != undefined){
-			if(asiento[i].dni == dniBuscar)
+		if(asientos[i].dni == dniBuscar)
 			buscarPasajeros += "<tr><td>"+asientos[i].apellido+"</td><td>"+asientos[i].nombre+"</td>"+
-								"<td>"+asientos[i].dni+"</td><td>"+asientos[i].asiento+"</td></tr>";
-		}
-		
-		
+							"<td>"+asientos[i].dni+"</td><td>"+asientos[i].nro_asiento+"</td></tr>";	
 	}
 	buscarPasajeros += "</table>";
 document.getElementById('listaPasajeros').innerHTML = buscarPasajeros;	
